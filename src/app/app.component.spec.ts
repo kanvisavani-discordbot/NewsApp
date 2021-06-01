59abf974d686d66a89c2a08d872cc938ae93ef11
Kanvi-discordbot <kanvisavani23@gmail.com>
2021-05-31 23:22:07 -0400

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NewsApp'`, () => {
+  it(`should have as title 'HatchWay'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NewsApp');
+    expect(app.title).toEqual('HatchWay');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('NewsApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('HatchWay app is running!');
   });
 });
